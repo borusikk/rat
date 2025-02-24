53d0697c3e75b72a1608567a9686561799d910bb
Borys <cherboris1997@gmail.com>
2025-02-24 20:46:56 +0200

    Update forms.py

--- feedback_app/forms.py
+++ feedback_app/forms.py
@@ -15,20 +15,20 @@ class FeedbackForm(forms.ModelForm):
     professionalism = forms.ChoiceField(
         choices=[(i, str(i)) for i in range(1, 6)],
         widget=forms.Select(attrs={'class': 'form-select'}),
-        label="Профессионализм (1-5)"
+        label="Професіоналізм (1-5)"
     )
     clarity = forms.ChoiceField(
         choices=[(i, str(i)) for i in range(1, 6)],
         widget=forms.Select(attrs={'class': 'form-select'}),
-        label="Понятность объяснения (1-5)"
+        label="Зрозумілість пояснення (1-5)"
     )
     attitude = forms.ChoiceField(
         choices=[(i, str(i)) for i in range(1, 6)],
         widget=forms.Select(attrs={'class': 'form-select'}),
-        label="Отношение к студентам (1-5)"
+        label="Ставлення до студентів (1-5)"
     )
     comment = forms.CharField(
         widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         required=False,
-        label="Комментарий"
+        label="Коментар"
     )
