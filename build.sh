303dbcf780bcc35638b9d3f84f16fc9de158187d
Borys <cherboris1997@gmail.com>
2025-02-25 08:33:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py import_professors exported_data/
 python manage.py import_professors exported_data/
+
+# Создание суперпользователя
+DJANGO_SUPERUSER_USERNAME=admin
+DJANGO_SUPERUSER_EMAIL=admin@example.com
+DJANGO_SUPERUSER_PASSWORD=admin
+
+python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --noinput || true
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | python manage.py shell
